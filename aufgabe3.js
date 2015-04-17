b754b9eba6cf8bcd2d1f2363fbea3a15cc36d6a5
Tobias Gerstenberg <Tobigers@gmail.com>
2015-04-17 13:28:14 +0200

    Aufgabe3 final

--- aufgabe3.js
+++ aufgabe3.js
@@ -1,56 +1,62 @@
+// Load required modules
 var fs = require('fs')
 var chalk = require('chalk')
 var skyscraper;
 
+//Read File 
 fs.readFile("wolkenkratzer.json", function(err, data){
 
+    //Check for errors
 	if(err) throw err;
 
+    //control message 
 	console.log('Daten werden eingelesen...');
-	console.log();
 
+    //Parse JSON and save into variable
 	skyscraper = JSON.parse(data);
     
-    //Sortieren 
+    //Sort Array, callback gives feedback when sorting is finished
     skyscraper.wolkenkratzer.sort(function (a,b){
         if(a.hoehe == b.hoehe){
             return 0;
         }
         
         else if(a.hoehe < b.hoehe){
-            return -1;
+            return 1;
         }
         
         else if(a.hoehe > b.hoehe){
-            return 1;
+            return -1;
         }
-    });
+    } , console.log("Sortieren beendet"));
     
-    //in Datei schreiben
+    //Specify the filename of the outputfile
     var outputFilename="wolkenkratzer_sortiert.json"
     
-    fs.writeFile(outputFilename, JSON.stringify(skyscraper, null, 4), function(err) {
-        if(err) {
-          console.log(err);
-        } 
+    //Write sorted array into a JSON file, Callback function 
+    fs.writeFile(outputFilename, JSON.stringify(data, null, 4), function(err) {
         
-        else {
-          console.log("JSON saved to " + outputFilename);
+        //Check for writing Errors
+        if(err) throw err;
+        
+        //control message when JSON is saved
+        console.log("JSON saved to " + outputFilename);
+        
+        //Start console output
+        console.log("ausgabe beginnt");
+    
+        for(var i = 0; i < skyscraper.wolkenkratzer.length; i++){
+            console.log(chalk.green('Name: '+skyscraper.wolkenkratzer[i].name));
+            console.log(chalk.cyan('Stadt: '+skyscraper.wolkenkratzer[i].stadt));
+            console.log(chalk.magenta('Hoehe: '+skyscraper.wolkenkratzer[i].hoehe+'m'));
+            console.log('--------------------')  
         }
-    }); 
     
+        console.log("ausgabe beendet");  
+    });     
+});	
+
+
     
-	for(var i = 0; i < skyscraper.wolkenkratzer.length; i++)
-	{
-		console.log(chalk.green('Name: '+skyscraper.wolkenkratzer[i].name));
-		console.log(chalk.cyan('Stadt: '+skyscraper.wolkenkratzer[i].stadt));
-		console.log(chalk.magenta('Hoehe: '+skyscraper.wolkenkratzer[i].hoehe+'m'));
-		console.log('--------------------')
-	}
-
-});
-
-
-
-
+   
   
