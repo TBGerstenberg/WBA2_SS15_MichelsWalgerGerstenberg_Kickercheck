930cfca23aa2d14227ef4076f24f5f3a2ce073d9
Tobias Gerstenberg <Tobigers@gmail.com>
2015-04-17 12:22:05 +0200

    Sorting added

--- aufgabe3.js
+++ aufgabe3.js
@@ -1,32 +1,46 @@
-//Include filesystem module
 var fs = require('fs')
+var chalk = require('chalk')
 
-// Variable to store parsed JSON
 var skyscraper;
 
-//Read the File at a spcified path , function in Parameters 
-//gets fired when the input is finished
 fs.readFile("wolkenkratzer.json", function(err, data){
 
 	if(err) throw err;
 
-	console.log('Datei wird eingelesen...');
+	console.log('Daten werden eingelesen...');
 	console.log();
-    
-    //data contains the content of the specified file,
-    // convert the JSON to a    JS object
+
 	skyscraper = JSON.parse(data);
     
+    
     //Sortieren 
+    skyscraper.wolkenkratzer.sort(function (a,b){
+        if(a.hoehe == b.hoehe){
+            return 0;
+        }
+        
+        else if(a.hoehe < b.hoehe){
+            return -1;
+        }
+        
+        else if(a.hoehe > b.hoehe){
+            return 1;
+        }
+    });
+    
+    
+    
     
 
-    //Loop over all Skyscrapers and display their properties
 	for(var i = 0; i < skyscraper.wolkenkratzer.length; i++)
 	{
-		console.log('Name: '+skyscraper.wolkenkratzer[i].name);
-		console.log('Stadt: '+skyscraper.wolkenkratzer[i].stadt);
-		console.log('Hoehe: '+skyscraper.wolkenkratzer[i].hoehe+'m');
+		console.log(chalk.green('Name: '+skyscraper.wolkenkratzer[i].name));
+		console.log(chalk.cyan('Stadt: '+skyscraper.wolkenkratzer[i].stadt));
+		console.log(chalk.magenta('Hoehe: '+skyscraper.wolkenkratzer[i].hoehe+'m'));
 		console.log('--------------------')
 	}
 
 });
+
+
+  
