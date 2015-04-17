828749263eaeec536b36ce70a44905b689a9668a
Tobias Gerstenberg <Tobigers@gmail.com>
2015-04-17 12:01:38 +0200

    Aufgabe 1 auskommentiert

--- aufgabe1.js
+++ aufgabe1.js
@@ -1,16 +1,23 @@
+//Include filesystem module
 var fs = require('fs')
 
+// Variable to store parsed JSON
 var skyscraper;
 
+//Read the File at a spcified path , function in Parameters 
+//gets fired when the input is finished
 fs.readFile("wolkenkratzer.json", function(err, data){
 
 	if(err) throw err;
 
 	console.log('Datei wird eingelesen...');
 	console.log();
-
+    
+    //data contains the content of the specified file,
+    // convert the JSON to a    JS object
 	skyscraper = JSON.parse(data);
 
+    //Loop over all Skyscrapers and display their properties
 	for(var i = 0; i < skyscraper.wolkenkratzer.length; i++)
 	{
 		console.log('Name: '+skyscraper.wolkenkratzer[i].name);
