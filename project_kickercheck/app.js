c270e36e7ba9809bfa8bed46071e89ffacb62f83
tolja <tolja@Retina-MBP.fritz.box>
2015-05-17 19:16:55 +0200

    Added Stylesheet, Response Code for malformed Request & Check if JSON is empty

--- project_kickercheck/app.js
+++ project_kickercheck/app.js
@@ -16,6 +16,8 @@ var data=[
 
 ]
 
+app.use(express.static(__dirname + '/styles'));
+
 //Repräsentation einer Spielerliste abrufen 
 app.get('/spielerprofil_liste',function(req,res){
     
@@ -24,10 +26,12 @@ app.get('/spielerprofil_liste',function(req,res){
     switch(acceptedTypes){
     
             case 'html':
-                var spielerProfilListe_html="";
+                var spielerProfilListe_html="<!DOCTYPE html><head> <link rel='stylesheet' type='text/css' href='style.css'/></head><body>";
                 for(var i=0; i<data.length ; i++){
+	              
                     spielerProfilListe_html+=profileToHTML(data[i]);
                 }
+                spielerProfilListe_html+="</body></html>";
                 res.type('html').send(spielerProfilListe_html);
             break;
             
@@ -57,14 +61,21 @@ app.post('/spielerprofil',jsonParser,function(req,res){
     console.log(user);
     
     // User Objekt in ein Array pushen 
+    if(req.body.name || req.body.email) {
     data.push(user);
+    }
+    else {
+	    res.status(400);
+res.send('Es wurden keine Daten abgesendet.');
+	    console.log("Keine Daten eingegeben");
+    }
 
     res.end();
 });
 
 //Funktion um ein Spielerprofil in eine HTML-Repräsentation umzuwandeln 
 function profileToHTML(profile){
-   var HTMLrep="<h1>"+profile.name+"</h1><br /><p>"+profile.email+"<p><br />";
+   var HTMLrep="<h1>"+profile.name+"</h1><p>"+profile.email+"</p>";
    return HTMLrep;
 }
 
