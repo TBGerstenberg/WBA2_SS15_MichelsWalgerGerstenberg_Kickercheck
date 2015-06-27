5657044318faa7f6dfb869e91598b8a57c12eb96
tolja <tolja@Retina-MBP.fritz.box>
2015-06-27 14:15:15 +0200

    Match PUT Dienstnutzer

--- project_kickercheck/user/user.js
+++ project_kickercheck/user/user.js
@@ -59,12 +59,18 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
-// about page 
+// match page 
 app.get('/Match', function(req, res) {
     res.render('pages/match');
 });
 
-app.post('/Match', jsonParser, function(req, res) {
+// match put page 
+app.get('/matchput', function(req, res) {
+    res.render('pages/matchput');
+});
+
+// Server Anfrage für ein Match POST
+app.post('/matchpost', jsonParser, function(req, res) {
 	
     // Speichert req.body
     var store = '';
@@ -153,6 +159,101 @@ app.post('/Match', jsonParser, function(req, res) {
     // Callback Funktion für Request an Server 
     matchAnlegen(function (responseString,responseLocation) {
 	    console.log(responseLocation);
+	    console.log(responseString);
+        res.write(responseString);
+        res.end();
+    });
+});
+
+
+app.put('/Match/:MatchId', jsonParser, function(req, res) {
+	
+    // Speichert req.body
+    var store = '';
+
+    function matchBearbeiten(callback) {
+
+	var responseLocation = '';
+    var responseString = '';
+
+    // HTTP Header setzen
+    var headers = {
+      'Content-Type': 'application/atom+xml'
+    };
+
+    // Mit Server verbinden
+    var options = {
+      host: 'localhost',
+      port: 3000,
+      path: '/Match/'+req.params.MatchId,
+      method: 'PUT',
+      headers: headers
+    };
+
+    // Request an Server
+    var anfrage = http.request(options, function(res){
+		
+      res.on('data', function(data) {
+        responseString += data;
+
+      });
+
+      res.on('end', function() {
+        callback(responseString);
+      });
+
+
+       anfrage.on('error', function(e) {
+        console.log(e);
+       });			
+    });
+
+
+    // AJAX Post Daten Allocate Funktion
+    function matchPut(callback) {
+
+        req.on('data', function(data) 
+        {
+            store += data;
+        });
+        req.on('end', function() 
+        {  
+           callback(store);
+        });    
+    }
+
+    // Callback Funktion für AJAX POST Daten Allocate
+    matchPut(function(store) {
+
+        var result = JSON.parse(store);
+
+    var match_object = {  
+       Datum: result.datum ,
+        Uhrzeit: result.uhrzeit ,
+   Austragungsort: [{ "link" : {'#text':' ', '@title':"Austragungsort",'@rel':LokalitaetRel,'@href':result.austragungsort} }] ,
+	'#list': [{"link" : {'#text':' ', '@title':"Teilnehmer 1",'@rel':BenutzerRel,'@href':result.spieler1 } },
+	{"link" : {'#text':' ', '@title':"Teilnehmer 2",'@rel':BenutzerRel,'@href':result.spieler2 } },
+	{"link" : {'#text':' ', '@title':"Teilnehmer 3",'@rel':BenutzerRel,'@href':result.spieler3 } },
+	{"link" : {'#text':' ', '@title':"Teilnehmer 4",'@rel':BenutzerRel,'@href':result.spieler4 } }]
+        };
+
+
+         var match_template = builder.create('Match',{version: '1.0', encoding: 'UTF-8'}).att("xmlns",kickerNS)
+    .ele(match_object)
+    .end({ pretty: true});
+
+//      console.log(util.inspect(match_template, {showHidden: false, depth: null}));
+
+
+        anfrage.write(match_template); 
+            anfrage.end();	   
+        });
+
+    }
+
+    // Callback Funktion für Request an Server 
+    matchBearbeiten(function (responseString) {
+
 	    console.log(responseString);
         res.write(responseString);
         res.end();
