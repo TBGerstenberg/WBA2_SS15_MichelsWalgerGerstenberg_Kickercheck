ab8c630194ddd5abb33a213b7a3bf7f4916606f4
Tobias Gerstenberg <Tobigers@gmail.com>
2015-08-30 15:49:49 +0200

    Statuscodes und Code aufgeräumt

--- project_kickercheck/Dienstgeber/routes/Servicedokument_Ressource.js
+++ project_kickercheck/Dienstgeber/routes/Servicedokument_Ressource.js
@@ -1,4 +1,8 @@
-//Liefert das Servicedokument,  dass die möglichen Interaktionsmöglichkeiten mit dem Service zeigt 
+var app = express.Router();
+
+//Liefert das Servicedokument,  dass die möglichen Interaktionsmöglichkeiten mit dem Service zeigt
+//Damit diese Ressource im Kontext von automatischer Service-Discovery und dem Ziel generischer Clients von Bedeutung ist 
+//hätte man hier auf ein standardisiertes Protokoll zurückgreifen müssen (z.B das AtomPub). 
 app.get('/',function(req,res){
     
     /*Interaktionsmöglichkeiten: 
@@ -8,12 +12,19 @@ app.get('/',function(req,res){
             -Turniere
             -Austragungsorte
             
-        -Links auf Relpages für diese Ressourcen ,die die zu übertragenen Repräsentaionen , sowie die definierten Verben 
-         auf den Ressourcen zeigen    
+        ggf.
+        Links auf Relpages für diese Ressourcen ,die die zu übertragenen Repräsentaionen , sowie die definierten Verben 
+        auf den Ressourcen zeigen       
     */
     
-
-
-
-
+    var service={
+        "BenutzerCollection":"http://localhost:3000/Benutzer",
+        "AustragungsortCollection":"http://localhost:3000/Austragungsort",
+        "MatchCollection":"http://localhost:3000/Match",
+        "TurnierCollection":"http://localhost:3000/Turnier",
+    }
+    
+    res.status(200).json(service).end();
 });
+
+module.exports = app;
--- project_kickercheck/Dienstgeber/routes/benutzer_ressource.js
+++ project_kickercheck/Dienstgeber/routes/benutzer_ressource.js
@@ -1,5 +1,61 @@
 var app = express.Router();
 
+//Liefert eine Collection aller existierenden Benutzer 
+//Hier werden ganze Repräsentationen übertragen , man hätte auch lediglich Links übertragen können .
+//Da die Benutzer jedoch IDs in ihren Repräsentationen haben , können die Locations ihre Locations darüber ermittelt werden 
+app.get('/',function(req,res){
+
+    //Headerfeld Accept abfragen
+    var acceptedTypes = req.get('Accept');
+
+    //Service kann bislang nur mit json-repräsentationen antworten 
+    switch (acceptedTypes) {
+
+            //client kann application/json verarbeiten     
+        case "application/json":
+
+            //Speichert die alle Benutzer
+            var response=[];    
+
+            //returned ein Array aller Keys die das Pattern Benutzer* matchen 
+            client.keys('Benutzer *', function (err, key) {
+
+                //Es gibt keine Benutzer in dieser Collection , liefere leeres array
+                if(key.length == 0) {
+                    //Dennoch war der Abruf erfolgreich , liefere leere Collection 
+                    res.status(200).json(response).end();
+                    return;
+                }
+
+                var sorted =  key.sort();
+
+                //Sortiere die Keys 
+                client.mget(sorted, function (err, benutzer) {
+
+                    //Frage alle diese Keys aus der Datenbank ab und pushe Sie in die Response
+                    benutzer.forEach(function (val) {
+
+                        var einNutzer = JSON.parse(val);
+
+                        //Benutzer soll nur ereichbare sein ,wenn er nicht gesperrt ist 
+                        if(einNutzer.isActive != 0) {
+
+                            //Pushe Daten 
+                            response.push(JSON.parse(val));
+                        }
+                    });
+
+                    //Abfrage war erfolgreich
+                    res.status(200).set("Content-Type","application/json").json(response).end();
+                });
+            });
+            break;
+
+        default:
+            res.status(406).end();
+            break;
+    }
+});
 
 //Liefert eine Repräsentation eines Benutzers mit <BenutzerId>
 app.get('/:BenutzerId', function(req, res) {
@@ -10,153 +66,98 @@ app.get('/:BenutzerId', function(req, res) {
     //Exists returns 0 wenn der angegebe Key nicht existiert, 1 wenn er existiert  
     client.exists('Benutzer ' + benutzerId, function(err, IdExists) {
 
-        client.mget('Benutzer ' + benutzerId, function(err, benutzerData) {
+        if(IdExists){
 
-            var benutzerObj=JSON.parse(benutzerData);
+            client.mget('Benutzer ' + benutzerId, function(err, benutzerData) {
 
-            //Es soll zukünftig möglich sein Benutzerseiten für den Zugriff von außen zu Sperren , daher das Flag "isActive" 
-            //in diesem Fall existiert der Benutzer und ist nicht gesperrt 
-            if(IdExists==1 && benutzerObj.isActive == 1) {
+                var benutzerObj=JSON.parse(benutzerData);
 
-                //Headerfeld Accept abfragen
-                var acceptedTypes = req.get('Accept');
+                //Es soll zukünftig möglich sein Benutzerseiten für den Zugriff von außen zu Sperren , daher das Flag "isActive" 
+                //in diesem Fall existiert der Benutzer und ist nicht gesperrt 
+                if(IdExists==1 && benutzerObj.isActive == 1) {
 
-                //Service kann bislang nur mit json-repräsentationen antworten 
-                switch (acceptedTypes) {
+                    //Headerfeld Accept abfragen
+                    var acceptedTypes = req.get('Accept');
 
-                    //client kann application/json verarbeiten     
-                    case "application/json":
+                    //Service kann bislang nur mit json-repräsentationen antworten 
+                    switch (acceptedTypes) {
 
-                        //Lese Benutzerdaten aus DB
-                        client.mget('Benutzer ' + benutzerId, function(err,benutzerdata){
+                            //client kann application/json verarbeiten     
+                        case "application/json":
 
-                            //Parse redis Antwort 
-                            var Benutzerdaten = JSON.parse(benutzerdata);
+                            //Lese Benutzerdaten aus DB
+                            client.mget('Benutzer ' + benutzerId, function(err,benutzerdata){
 
-                            //Setze Contenttype der Antwort auf application/json, zeige mit 200-OK erfolg 
-                            res.set("Content-Type", 'application/json').status(200).json(Benutzerdaten).end();
-                        });
-                        break;
+                                //Parse redis Antwort 
+                                var Benutzerdaten = JSON.parse(benutzerdata);
 
-                    default:
-                        //Der gesendete Accept header enthaelt kein unterstuetztes Format , 406 - Notacceptable 
-                        //Includiere Servicedokument oder Benutzercollection Link um dem Client einen Hinweis zu geben wie das Problem 
-                        //zu beheben ist.  
-                        var benutzerRel={
-                            "href":"/Benutzer",
-                        };
-                        res.status(406).json(benutzerRel).end();
-                        break;
+                                //Setze Contenttype der Antwort auf application/json, zeige mit 200-OK erfolg 
+                                res.set("Content-Type", 'application/json').status(200).json(Benutzerdaten).end();
+                            });
+                            break;
+
+                        default:
+                            //Der gesendete Accept header enthaelt kein unterstuetztes Format , 406 - Notacceptable 
+                            //Includiere Servicedokument oder Benutzercollection Link um dem Client einen Hinweis zu geben wie das Problem 
+                            //zu beheben ist.  
+                            var benutzerRel={
+                                "href":"/Benutzer",
+                            };
+                            res.status(406).json(benutzerRel).end();
+                            break;
+                    }
                 }
-            }
-            
-            //Ressource für den Zugriff von außen gesperrt ,aber vorhanden 
-            else if(IdExists == 1 && benutzerObj.isActive == 0) {
-                res.status(404).send("Die Ressource wurde für den Zugriff von außen gesperrt.").end();
-            }
-            
-            //Ressource nicht gefunden 
-            else {
-                res.status(404).send("Die Ressource wurde nicht gefunden.").end();
-            }
 
-        });
+                //Ressource für den Zugriff von außen gesperrt ,aber vorhanden , hier könnte in der Zukunft 
+                //etwas anderes als 404 passieren 
+                else if(IdExists == 1 && benutzerObj.isActive == 0) {
+                    res.status(404).end();
+                }
+            });
+        }
+
+        //Ressource nicht gefunden 
+        else {
+            res.status(404).end();
+        }
     });
 });
 
 
-app.post('/', function(req, res) {
-
-    //Content Type der Anfrage abfragen 
-    var contentType = req.get('Content-Type');
-
-    //Check ob der Content Type der Anfrage json ist 
-    if (contentType != "application/json") {
-        res.set("Accepts", "application/json").status(406).send("Content Type is not supported").end(); 
-    } 
-
-    else{
-
-
-        var Benutzer=req.body;
-        //Pflege Daten aus Anfrage in die DB ein
-
-        // BenutzerId in redis erhöhen, atomare Aktion 
-        client.incr('BenutzerId', function(err, id) {
-            console.log("Die BenutzerId nach hinzufügen eines Benutzers : " + id);
-
-            var benutzerObj={
-                'id' : id,
-                'Name': Benutzer.Name,
-                'Alter': Benutzer.Alter,
-                'Bild': Benutzer.Bild,
-                'isActive': 1
-            };
-
-            client.set('Benutzer ' + id, JSON.stringify(benutzerObj));
-
-            //Setze Contenttype der Antwort auf application/atom+xml
-            res.set("Content-Type", 'application/json').set("Location", "/Benutzer/" + id).status(201).json(benutzerObj).end();
-
-
-        });
-    }
-});
-
+//Ändert die Informationen eines Benutzers 
 app.put('/:BenutzerId', function(req, res) {
 
     var contentType = req.get('Content-Type');
 
-    //Wenn kein json geliefert wird antwortet der Server mit 406- Not acceptable und zeigt über accepts-Header gütlige ContentTypes 
+    //Wenn kein json geliefert wird antwortet der Server mit 415-unsupported media type und zeigt über accepts-Header gütlige ContentTypes 
     if (contentType != "application/json") {
-        res.set("Accepts", "application/json").status(406).send("Content Type is not supported").end(); 
+        res.set("Accepts", "application/json").status(415).end(); 
     } 
 
+    //Content type war OK
     else {
-
         var benutzerId = req.params.BenutzerId;
 
         //Exists returns 0 wenn der angegebe Key nicht existiert, 1 wenn er existiert  
         client.exists('Benutzer ' + benutzerId, function(err, IdExists) {
-
-            /*
-
-	TODO
-
-            //Checke ob bestehender EIntrag gelöscht bzw für den Zugriff von außen gesperrt wrude 
-            client.hget('Benutzer ' + benutzerId, "isActive", function(err, benutzerValid) {
-
-                //client.exists hat false geliefert 
-                if (IdExists == 1 && benutzerValid == 0) {
-                    res.status(404).send("Die Ressource wurde nicht gefunden.");
-                    res.end();
-                }
-
-                //Der Benutzer existiert und kann bearbeitet werden 
-                else if (IdExists == 1 && benutzerValid == 1) {
-
-
-                }                  
-            });      
-*/ 
 
             //client.exists hat false geliefert 
             if (!IdExists) {
                 res.status(404).end();
             } 
 
-
+            //Benutzer ist vorhanden 
             else {
 
-                //Lese aktuellen Zustand des Turniers aus DB
+                //Lese aktuellen Zustand des Benutzers aus der DB 
                 client.mget('Benutzer '+benutzerId,function(err,benutzerdata){
 
+                    //Parse Redis Antwort 
                     var Benutzerdaten = JSON.parse(benutzerdata);
 
                     //Aktualisiere änderbare Daten 
                     Benutzerdaten.Name = req.body.Name;
                     Benutzerdaten.Alter = req.body.Alter;
-
 
                     //Schreibe Turnierdaten zurück 
                     client.set('Benutzer ' + benutzerId,JSON.stringify(Benutzerdaten));
@@ -169,78 +170,82 @@ app.put('/:BenutzerId', function(req, res) {
     }
 });
 
+//Fügt der Benutzer/ Collection ein neues Element hinzu 
+app.post('/', function(req, res) {
+
+    //Content Type der Anfrage abfragen 
+    var contentType = req.get('Content-Type');
+
+    //Check ob der Content Type der Anfrage json ist 
+    if (contentType != "application/json") {
+        res.set("Accepts", "application/json").status(415).end(); 
+    } 
+
+    //Content type war in Ordnung (application/json)
+    else{
+
+        //Lese request body aus 
+        var Benutzer=req.body;
+
+        // BenutzerId in redis erhöhen, atomare Aktion 
+        client.incr('BenutzerId', function(err, id) {
+
+            //JSON Objekt das als String in redis eingefügt wird 
+            var benutzerObj={
+                'id' : id,
+                'Name': Benutzer.Name,
+                'Alter': Benutzer.Alter,
+                'Bild': Benutzer.Bild,
+                'isActive': 1
+            };
+
+            //Füge JSON-String in DB ein 
+            client.set('Benutzer ' + id, JSON.stringify(benutzerObj));
+
+            //Setze Contenttype der Antwort auf application/json und sende 201-created bei erfolgreichem POST
+            //Includiere Location Header mit URL der neu angelegten Ressource
+            res.set("Content-Type", 'application/json').set("Location", "/Benutzer/" + id).status(201).json(benutzerObj).end();
+        });
+    }
+});
+
+//Löscht einen Benutzer aus dem System 
 app.delete('/:BenutzerId', function(req, res) {
 
     var benutzerId = req.params.BenutzerId;
 
+    //Gibt des den Benutzer mit dieser Id?
     client.exists('Benutzer ' + benutzerId, function(err, IdExists) {
 
-        client.mget('Benutzer ' + benutzerId, function(err, benutzerData) {
+        //Der Benutzer existiert 
+        if(IdExists){
 
-            var benutzerObj=JSON.parse(benutzerData);
+            //Benutzerdaten sollen für statistische Zwecke auch beim löschen erhalten 
+            //bleiben , da hier evtl. noch Informationen über gewonnene Matches o.Ä eingepflegt werden 
+            client.mget('Benutzer ' + benutzerId, function(err, benutzerData) {
 
-            console.log("Existiert die id?"+IdExists + typeof(IdExists));
-            console.log("Ist der benutzer aktiv?"+benutzerObj.isActive + typeof(benutzerObj.isActive));
-            console.log("Der key des Benutzers:" + 'Benutzer ' + benutzerId);
+                //Parse redis antwort 
+                var benutzerObj=JSON.parse(benutzerData);
 
-            if(IdExists==1 && benutzerObj.isActive ==1) {
-                //Setze das isActive Attribut des Benutzers in der Datenbank auf 0 , so bleiben seine Daten für statistische Zwecke erhalten , nach 				   
-                //außen ist die Ressource aber nicht mehr erreichbar 
-                console.log("Hier spring ich hoffentlich rein ");
+                //Benutzer existiert und ist nicht gesperrt 
+                if(IdExists==1 && benutzerObj.isActive ==1) {
 
+                    //Sperre Benutzer für Zugriff von außen 
+                    benutzerObj.isActive=0;
 
+                    //Schreibe Benutzerdaten zurück 
+                    client.set('Benutzer ' + benutzerId,JSON.stringify(benutzerObj));
 
-                benutzerObj.isActive=0;
-                console.log(benutzerObj);
-                //Schreibe Turnierdaten zurück 
-                client.set('Benutzer ' + benutzerId,JSON.stringify(benutzerObj));
-
-                //Alles ok , sende 200 
-                res.set("Content-Type", 'application/json').status(200).json(benutzerObj).end();
-
-
-            }
-
-            //Es gibt keinen Benutzer mit dieser id
-            else {
-                res.status(404).end();
-            }
-        });
-    });
-});
-
-app.get('/',function(req,res){
-
-    //Speichert die alle Benutzer
-    var response=[];    
-
-    //returned ein Array aller Keys die das Pattern Benutzer* matchen 
-    client.keys('Benutzer *', function (err, key) {
-
-        if(key.length == 0) {
-            res.json(response);
-            return;
+                    //Alles ok , sende 200-Ok für erfolgreiches löschen und liefere eine repräsentation des gelöschten Benutzers 
+                    res.set("Content-Type", 'application/json').status(200).json(benutzerObj).end();
+                }
+            });
         }
 
-        var sorted =  key.sort();
-
-        client.mget(sorted, function (err, benutzer) {
-
-            //Frage alle diese Keys aus der Datenbank ab und pushe Sie in die Response
-            benutzer.forEach(function (val) {
-
-                var einNutzer = JSON.parse(val);
-
-                if(einNutzer.isActive != 0) {
-
-                    response.push(JSON.parse(val));
-                }
-
-            });
-            res.status(200).set("Content-Type","application/json").json(response).end();
-
-        });
-
+        //Es gibt keinen Benutzer mit dieser id
+        else {
+            res.status(404).end();
+        }
     });
 });
 
